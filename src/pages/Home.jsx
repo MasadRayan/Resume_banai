34dfa4effdd70e71c55ef8856a2990984eb0081d
Masad Rayan <masadrayan2002@gmail.com>
2026-03-05 03:13:17 +0600

    Created Banner for the top

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from '../components/Home/Banner';
 
 const Home = () => {
   return (
     <div>
-      <h1 className='mx-auto text-center text-5xl'>Hi from Homme</h1>
+      <Banner></Banner>
     </div>
   )
 }
