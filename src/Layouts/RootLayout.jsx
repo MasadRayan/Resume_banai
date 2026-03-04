87c45a41c5731e7a78bcaa3f6f43eab6ef372fb1
Masad Rayan <masadrayan2002@gmail.com>
2026-03-05 04:09:20 +0600

    Navbar Edited successful

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Banner from '../components/Home/Banner'
 
 const RootLayout = () => {
   return (
     <div>
+      <Banner></Banner>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
