639cd25424b41f76c010b2b86e81459abfa2cfbe
Masad Rayan <masadrayan2002@gmail.com>
2026-03-05 01:52:08 +0600

    Added some asset in the Project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 className='font-'>Hi from Homme</h1>
+      <h1 className='mx-auto text-center text-5xl'>Hi from Homme</h1>
     </div>
   )
 }
