76087ec741719d806848c8f1f52c61eb5202c4a0
Masad Rayan <masadrayan2002@gmail.com>
2026-03-05 17:44:32 +0600

    Call to action section createxd and added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Home/Hero';
 import FeaturedSection from '../components/Home/FeaturedSection';
 import Testimonial from '../components/Home/Testimonial';
+import CTASection from '../components/Home/CTASection';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero></Hero>
       <FeaturedSection></FeaturedSection>
       <Testimonial></Testimonial>
+      <CTASection></CTASection>
     </div>
   )
 }
